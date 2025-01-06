6c895a2ec2bd4ec05e91af426f3a003dd262eba0
jdatsenko <yu.datsenko@gmail.com>
2025-01-06 01:15:30 +0100

    props destructuring

---
+++ part1/partc/src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from './Hello'
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App
---
+++ part1/partc/src/Hello.jsx
@@ -0,0 +1,13 @@
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+  return (
+    <>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </>
+  );
+}
+
+export default Hello
