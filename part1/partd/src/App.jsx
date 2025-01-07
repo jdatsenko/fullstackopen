0727d840164b6e85a18b6914aa68672f080ab752
jdatsenko <yu.datsenko@gmail.com>
2025-01-07 14:27:39 +0100

    array handling

--- part1/partd/src/App.jsx
+++ part1/partd/src/App.jsx
@@ -2,9 +2,19 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [clicks, setClicks] = useState({left: 0, right: 0})
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
-  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
+  const handleLeftClick = () => {
+    setClicks({...clicks, left: clicks.left + 1})
+    setAll(allClicks.concat('L'))
+    setTotal(clicks.left + clicks.right)
+  }
+  const handleRightClick = () => {
+    setClicks({...clicks, right: clicks.right + 1})
+    setAll(allClicks.concat('R'))
+    setTotal(clicks.left + clicks.right)
+  }
 
   return (
     <>
@@ -12,6 +22,10 @@ const App = () => {
       <button onClick = {handleLeftClick}>left</button>
       <button onClick = {handleRightClick}>right</button>
       {clicks.right}
+
+      {allClicks.join(' ')}
+      {/* one less, because of the async */}
+      <p>total {total}</p>
     </>
   )
 }
