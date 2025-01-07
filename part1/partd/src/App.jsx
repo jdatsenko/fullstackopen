933b2923029a712512d22054b26b0d486c006b5d
jdatsenko <yu.datsenko@gmail.com>
2025-01-07 14:30:47 +0100

    conditional rendering

--- part1/partd/src/App.jsx
+++ part1/partd/src/App.jsx
@@ -1,31 +1,26 @@
 import React, { useState } from 'react'
+import History from './History'
 
 const App = () => {
   const [clicks, setClicks] = useState({left: 0, right: 0})
   const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setClicks({...clicks, left: clicks.left + 1})
     setAll(allClicks.concat('L'))
-    setTotal(clicks.left + clicks.right)
   }
   const handleRightClick = () => {
     setClicks({...clicks, right: clicks.right + 1})
     setAll(allClicks.concat('R'))
-    setTotal(clicks.left + clicks.right)
   }
 
   return (
     <>
       {clicks.left}
-      <button onClick = {handleLeftClick}>left</button>
-      <button onClick = {handleRightClick}>right</button>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
       {clicks.right}
-
-      {allClicks.join(' ')}
-      {/* one less, because of the async */}
-      <p>total {total}</p>
+      <History allClicks={allClicks} />
     </>
   )
 }
