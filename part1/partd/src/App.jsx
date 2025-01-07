5628d5142ddcdc4931ad959724fa2c8454a3b457
jdatsenko <yu.datsenko@gmail.com>
2025-01-07 14:25:50 +0100

    complex state

---
+++ part1/partd/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [clicks, setClicks] = useState({left: 0, right: 0})
+
+  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
+  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
+
+  return (
+    <>
+      {clicks.left}
+      <button onClick = {handleLeftClick}>left</button>
+      <button onClick = {handleRightClick}>right</button>
+      {clicks.right}
+    </>
+  )
+}
+
+export default App
