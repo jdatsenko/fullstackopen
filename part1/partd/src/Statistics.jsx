fd03a63fbcc310d91455694f94c18d2db1ebb488
jdatsenko <yu.datsenko@gmail.com>
2025-01-09 09:35:39 +0100

    unicafe step 2

--- part1/partd/src/Statistics.jsx
+++ part1/partd/src/Statistics.jsx
@@ -1,10 +1,18 @@
+import React, { useState } from 'react'
+
 const Statistics = ({good, neutral, bad}) => {
+  const all = good + bad + neutral
+  const positivePercentage = all > 0 ? (good * 100) / all : 0;
+
   return (
     <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {good * 1 + bad * -1}</p>
+      <p>positive {positivePercentage.toFixed(1)}</p>
     </>
   );
 };
