2073eac4cd2388ba10c6175049219a55c46a04f4
jdatsenko <yu.datsenko@gmail.com>
2025-01-09 15:03:15 +0100

    anecdotes step 1

--- part1/partd/src/App.jsx
+++ part1/partd/src/App.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from 'react'
-import Statistics from './Statistics'
 import Button from './Button'
 
 const App = () => {
-  const [clicks, setClicks] = useState({good: 0, neutral: 0, bad: 0})
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  const handleGoodClick = () => {
-    setClicks({...clicks, good: clicks.good + 1})
-  }
-
-  const handleNeutralClick = () => {
-    setClicks({...clicks, neutral: clicks.neutral + 1})
-  }
-
-  const handleBadClick = () => {
-    setClicks({...clicks, bad: clicks.bad + 1})
+  const changeAnecdote = () => {
+    const random = Math.floor(Math.random() * 8);
+    setSelected(random)
   }
 
   return (
-    <>
-      <h1>give feedback</h1>
-      <Button handleClick={handleGoodClick} text="good" />
-      <Button handleClick={handleNeutralClick} text="neutral" />
-      <Button handleClick={handleBadClick} text="bad" />
-      <Statistics good={clicks.good} neutral={clicks.neutral} bad={clicks.bad} />
-    </>
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={changeAnecdote} text="next anecdote" />
+    </div>
   )
 }
 
