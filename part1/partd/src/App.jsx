5c5b9508030c492233aa9c650ad13eee3611be6b
jdatsenko <yu.datsenko@gmail.com>
2025-01-10 14:41:40 +0100

    anecdotes step 2 , 3

--- part1/partd/src/App.jsx
+++ part1/partd/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import Button from './Button'
+import { useState } from "react";
+import Button from "./Button";
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  ]
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  ];
   const [selected, setSelected] = useState(0);
 
   const [points, setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0 });
-
-  // const points = { 0: 0, 1: 0, 2: 0, 3: 0 };
 
   const voteAnecdote = () => {
     const copy = { ...points };
@@ -22,8 +20,15 @@ const App = () => {
 
   const changeAnecdote = () => {
     const random = Math.floor(Math.random() * 4);
-    setSelected(random)
-  }
+    setSelected(random);
+  };
+
+  const mostVotes = Math.max(...Object.values(points));
+
+  const getMostVotes = () => {
+    const mostIndex = Object.keys(points).find((key) => points[key] == mostVotes);
+    return mostIndex;
+  };
 
   return (
     <div>
@@ -31,8 +36,14 @@ const App = () => {
       <p>has points: {points[selected]}</p>
       <Button handleClick={changeAnecdote} text="next anecdote" />
       <Button handleClick={voteAnecdote} text="vote" />
+      {mostVotes > 0 && (
+        <div>
+          <p>Anecdote with most votes</p>
+          <p>{anecdotes[getMostVotes()]}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App
+export default App;
